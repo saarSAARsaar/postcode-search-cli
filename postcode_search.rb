16fd8d96b524a961aafdcfc5c5bfa270d990fdf9
Sarthak Arora <sarthakarora@Kintsugi.local>
2020-07-09 15:09:53 +0200

    2nd excercise Day 2

--- postcode_search.rb
+++ postcode_search.rb
@@ -1,7 +1,12 @@
 require 'csv'
 
+puts "Enter a postcode"
+input = gets.chomp
 row_count = 0
 CSV.parse(File.read("Downloads/plz_verzeichnis_v2.csv"), headers: true, col_sep: ";") do |row|
     row_count += 1
+    if row["postleitzahl"] == input
+        puts row["kanton"] + ", " + row["ortbez18"]
+    end
 end
 puts row_count
