d8d4aea438d69d429e03049cbd25b07ce6a866e6
Sarthak Arora <sarthakarora@Kintsugi.local>
2020-07-09 15:18:53 +0200

    8th excercise Day 2

--- postcode_search.rb
+++ postcode_search.rb
@@ -3,31 +3,39 @@ require 'byebug'
 
 data_path = "Downloads/plz_verzeichnis_v2.csv"
 
-postcodes = CSV.parse(File.read(data_path), headers: true, col_sep: ";")
-puts "Number of rows: " + postcodes.length.to_s
+class Place
+    attr_reader :name, :postcode, :canton
+
+    def initialize(postcode, name, canton)
+        @postcode = postcode
+        @name = name
+        @canton = canton
+    end 
+end
+
+rows = CSV.parse(File.read(data_path), headers: true, col_sep: ";")
+places = rows.map do |row|
+    Place.new(row["postleitzahl"], row["ortbez18"], row["kanton"])
+end
+
+puts "Number of rows: " + places.length.to_s
 puts ''
 
 running = true
 while running
     puts "Enter a postcode or a name of city/town"
     col_val_to_find = gets.chomp
-    if !/\A\d+\z/.match(col_val_to_find)
-        puts "Checking for the postcode of that place..."
-        col_to_check = "ortbez18"
-        col_to_return = "postleitzahl"
+    
+    if /\A\d+\z/.match(col_val_to_find) 
+        wanted_places = places.find_all { |place| place.postcode == col_val_to_find }
+        return_vals = wanted_places.map { |place| place.canton }
     else
-        puts "Checking for the canton of that postcode..."
-        col_to_check = "postleitzahl"
-        col_to_return = "kanton"
+        wanted_places = places.find_all { |place| place.name == col_val_to_find }
+        return_vals = wanted_places.map { |place| place.postcode }
     end  
-    return_vals = []
-
-    rows = postcodes.find_all { |row| row[col_to_check] == col_val_to_find }
-    return_vals = rows.map { |row| row[col_to_return] }
 
     puts ''
     if return_vals.length == 0
-        # puts "The postcode #{postcode} was not found"
         puts "No values were found for #{col_val_to_find}"
     else
         puts return_vals.uniq
