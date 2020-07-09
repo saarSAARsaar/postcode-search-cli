00f324716933e477094501f04560f4f73450eb8a
Sarthak Arora <sarthakarora@Kintsugi.local>
2020-07-09 15:17:14 +0200

    4th excercise Day 2

--- postcode_search.rb
+++ postcode_search.rb
@@ -1,25 +1,45 @@
 require 'csv'
 require 'byebug'
 
-puts "Enter a postcode"
-postcode = gets.chomp
-cantons = []
+data_path = "Downloads/plz_verzeichnis_v2.csv"
+
 row_count = 0
-CSV.parse(File.read("Downloads/plz_verzeichnis_v2.csv"), headers: true, col_sep: ";") do |row|
+CSV.parse(File.read(data_path), headers: true, col_sep: ";") do |row|
     row_count += 1
-    if row["postleitzahl"] == postcode
-        cantons.append(row["kanton"])
+end
+puts "Number of rows: " + row_count.to_s 
+puts ''
+
+running = true
+while running
+    puts "Enter a postcode"
+    postcode = gets.chomp
+    cantons = []
+
+    CSV.parse(File.read(data_path), headers: true, col_sep: ";") do |row|
+        row_count += 1
+        if row["postleitzahl"] == postcode
+            cantons.append(row["kanton"])
+        end
     end
+
+    puts ''
+    if cantons.length == 0
+        puts "The postcode #{postcode} was not found"
+    else
+        filtered_cantons = []
+        cantons.each do |canton|
+            if !filtered_cantons.include? canton
+                filtered_cantons.append(canton)
+            end
+        end
+        puts filtered_cantons
+    end
+
+    puts "\nDo you want to end the program? Type exit if so"
+    should_end = gets.chomp
+    if should_end.downcase == 'exit'
+        running = false
+    end
+    puts ''
 end
-if cantons.length == 0
-    puts "The postcode #{postcode} was not found"
-else
-    # filtered_cantons = []
-    # cantons.each do |canton|
-    #    if !filtered_cantons.include? canton
-    #        filtered_cantons.append(canton)
-    #    end
-    # end
-    puts cantons.uniq
-end
-puts row_count
